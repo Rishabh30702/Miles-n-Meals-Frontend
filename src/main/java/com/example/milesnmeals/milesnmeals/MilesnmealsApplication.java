5dab8ca3bb1d7c8450039385053db85d647ab97f
amchangama <rishabh.singh@valliento.tech>
2025-12-23 15:44:41 +0530

    initial commit

---
+++ src/main/java/com/example/milesnmeals/milesnmeals/MilesnmealsApplication.java
@@ -0,0 +1,13 @@
+package com.example.milesnmeals.milesnmeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilesnmealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilesnmealsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/milesnmeals/milesnmeals/MilesnmealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.milesnmeals.milesnmeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilesnmealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
